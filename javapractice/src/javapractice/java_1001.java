30a7c8bc2b1f81497d68b921b42201b50fdcfca9
hoeng <hello.git.thdghdud317@naver.com>
2023-07-26 13:39:35 +0900

    자바 백준

---
+++ javapractice/src/javapractice/java_1001.java
@@ -0,0 +1,18 @@
+package javapractice;
+import java.util.Scanner;
+
+
+public class java_1001 {
+
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A-B);
+
+		in.close();
+	}
+}
